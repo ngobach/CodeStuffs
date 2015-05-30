bc0e7498bce44ad169d4c9b2f55fee34e0a61299
DDrax-Z <vip9xpro312@gmail.com>
2015-05-30 14:11:59 +0700

    Code by DDraxZ

--- OOP/final/Bai 6 by DDraxZ.cpp
+++ OOP/final/Bai 6 by DDraxZ.cpp
@@ -41,34 +41,63 @@ public:
 
 int Thisinh::dem = 110;
 
+
+class dsts
+{
+    private:
+        Thisinh dsts[100];
+        int n;
+    public:
+        void Nhapds()
+        {
+            int  i;
+            cout << "Nhap so thi sinh can nhap: ";
+            cin >> n;
+            for (i=0;i<n;i++)
+            {
+                cout << "Nhap thi sinh thu " << i+1 << endl;
+                dsts[i].Input();
+            }
+        }
+
+        void Hthids()
+        {
+            cout << "\n\nDanh sach thi sinh da nhap vao: \n";
+            for (int i=0;i<n;i++)
+                dsts[i].Output();
+        }
+
+        void Sapxep()
+        {
+            int i,j;
+            Thisinh temp;
+            for (i=0;i<n-1;i++)
+                for (j=i+1;j<n;j++)
+                    if(dsts[i].getTong()>dsts[j].getTong())
+                        {
+                            temp = dsts[i];
+                            dsts[i]=dsts[j];
+                            dsts[j]=temp;
+                        }
+                cout << "\n\nDanh sach sinh vien sap xep theo tong diem tang dan: \n";
+                    for (i=0;i<n;i++)
+                        dsts[i].Output();
+        }
+
+        void Hthids2()
+        {
+             cout << "\n\nDanh sach sinh vien co tong diem tren 18: \n";
+                for (int i=0;i<n;i++)
+                    if(dsts[i].getTong()>=18)
+                        dsts[i].Output();
+        }
+};
+
 main()
 {
-    Thisinh A[100], temp;
-    int i, n, j;
-    cout << "Nhap so thi sinh can nhap: ";
-    cin >> n;
-    for (i=0;i<n;i++)
-        A[i].Input();
-
-    for (i=0; i<n; i++)
-        A[i].Output();
-
-    temp = A[0];
-    for (i=0;i<n-1;i++)
-        for (j=i+1;j<n;j++)
-            if(A[i].getTong()>A[j].getTong())
-                {
-                    temp = A[i];
-                    A[i]=A[j];
-                    A[j]=temp;
-                }
-
-    cout << "\n\nDanh sach sinh vien sap xep theo tong diem tang dan: \n";
-    for (i=0;i<n;i++)
-        A[i].Output();
-
-    cout << "\n\nDanh sach sinh vien co tong diem tren 18: \n";
-    for (i=0;i<n;i++)
-        if(A[i].getTong()>=18)
-            A[i].Output();
+    dsts A;
+    A.Nhapds();
+    A.Hthids();
+    A.Sapxep();
+    A.Hthids2();
 }
