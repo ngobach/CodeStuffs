3bb67cbbc017b2ec9960f3e0ef07c7c31f5c8366
thanbaiks <thanbaiks@gmail.com>
2015-06-15 15:19:15 +0700

    Danh sach lien ket

---
+++ ctdlgt/dslk.cpp
@@ -0,0 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+	int val;
+	Node* next;
+};
+
+class LinkedList{
+	private:
+		int count;
+		Node* head;
+	public:
+		LinkedList();
+		void insert(int);
+		void insertAt(int,int);
+		void deleteAt(int);
+		void display();
+		int get(int);
+		int getCount();
+		bool isEmpty();
+};
+
+LinkedList::LinkedList(){
+	count = 0;
+	head = NULL;
+}
+
+void LinkedList::insert(int val){
+	Node* n = new Node();
+	n->val = val;
+	n->next = NULL;
+	
+	if (isEmpty()){
+		head = n;
+	}else{
+		Node *t = head;
+		while (t->next != NULL) t = t->next;
+		t->next = n;
+	}
+	count++;
+}
+
+int LinkedList::getCount(){
+	return count;
+}
+
+void LinkedList::deleteAt(int pos){
+	if (pos < 0 || pos >= count)
+		return;
+	Node *t;
+	t = head;
+	if (pos == 0){
+		head = head->next;
+		delete t;
+	}else{
+		while (--pos) t = t->next;
+		Node *n = t->next;
+		t->next = n->next;
+		delete n;
+	}
+	count--;
+}
+
+void LinkedList::display(){
+	Node *n = head;
+	while (n!=NULL){
+		cout << n->val << ", ";
+		n = n->next;
+	}
+	cout << endl;
+}
+
+void LinkedList::insertAt(int pos,int val){
+	if (pos < 0 || pos > count)
+		return;
+	Node* n = new Node();
+	n->val = val;
+	n->next = NULL;
+	if (pos == 0){
+		// chen dau
+		n->next = head;
+		head = n;
+	}else{
+		// chen giua
+		Node *t = head;
+		while (--pos) t = t->next;
+		n->next = t->next;
+		t->next = n;
+	}
+	count++;
+}
+
+int LinkedList::get(int pos){
+	if (pos < 0 || pos >= count)
+		return INT_MIN;
+	Node *t = head;
+	while (pos--) t = t->next;
+	return t->val;
+}
+
+bool LinkedList::isEmpty(){
+	return count == 0;
+}
+
+
+
+
+
+// ============ tests
+void test1(){
+	LinkedList ll;
+	ll.insert(1);
+	ll.insert(3);
+	ll.insert(2);
+	ll.insert(5);
+	ll.insert(6);
+	ll.display();
+}
+void test2(){
+	LinkedList ll;
+	ll.insert(1);
+	ll.insert(3);
+	ll.insert(2);
+	ll.insert(5);
+	ll.insert(6);
+	ll.display();
+	ll.deleteAt(0);
+	ll.deleteAt(9999);
+	ll.deleteAt(-100);
+	ll.deleteAt(2);
+	ll.display();
+}
+void test3(){
+	LinkedList ll;
+	ll.insert(1);
+	ll.insert(3);
+	ll.insert(2);
+	ll.insert(5);
+	ll.insert(6);
+	cout << "Phan tu thu 4: " << ll.get(4) << endl;
+}
+int main(){
+	cout << "TEST1:" << endl;test1();
+	cout << "TEST2:" << endl;test2();
+	cout << "TEST3:" << endl;test3();
+	return 0;
+}
+
