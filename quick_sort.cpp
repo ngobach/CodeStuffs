c97862650f324ae59a0edcf31913d4d961b79d30
ThanBaiKS <thanbaiks@gmail.com>
2015-06-29 16:45:53 +0700

    Quick Sort

---
+++ quick_sort.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+void qs(int arr[],int first,int last){
+	if (last-first <= 1)
+		return;
+	int pivot = (last-first)/2;
+
+}
+
+int main(){
+	int arr[] = {1,5,2,6,2,3,7,8,11,32,1,2,4,1,6,3,7,3,43,324,656,324,65,324,43,54};
+	return 0;
+}
+
